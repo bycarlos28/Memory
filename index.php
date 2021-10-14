3d0d2efcb3c2786478363fa223226ca331b6671e
bycarlos28 <carlosfernandez28correa@gmail.com>
2021-10-14 19:09:21 +0200

    created index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to Memory</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div id="body">
+		<img src="Media/Images/logoMemory.png">
+		<div id="Instructions">
+			<h1>Instructions</h1>
+			<ul>
+				<li><p>To turn a card you have to click on it</p></li>
+				<li><p>There can only be two cards turned</p></li>
+				<li><p>If the two cards are equal they will remain turned</p></li>
+				<li><p>If the two cards are different, they will remain for 2 seconds turned</p></li>
+				<li><p>Time and attempts are unlimited</p></li>
+			</ul>
+		</div>
+	</div>
+	<div id="button">
+		<form action="game.php">
+			<button>PLAY</button>
+		</form>
+	</div>
+</body>
+</html>

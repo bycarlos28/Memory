cd4e2c7fd513387c669184132a5c33dd42b16973
CarlosMunozRo <carmuny54@gmail.com>
2021-10-18 16:24:00 +0200

    Added new Css

--- index.php
+++ index.php
@@ -4,11 +4,18 @@
 	<meta charset="utf-8">
 	<title>Welcome to Memory</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+
 </head>
 <body>
+
+
+
+	<div id="wheel" class="steering"></div>
 	<div id="body">
 		<img src="Media/Images/logoMemory.png">
 		<div id="Instructions">
+			
 			<h1>Instructions</h1>
 			<ul>
 				<li><p>To turn a card you have to click on it</p></li>
@@ -24,5 +31,44 @@
 			<button>PLAY</button>
 		</form>
 	</div>
+
+	<div class="wrap">
+</div>
+</div>
+
+<div class="ocean"></div>
+<div class="ocean-overlay"></div>
+<div class="here-there">
+  <div class="traslate">
+    <div class="ship">
+      <div class="bottom-1">
+        <div class="bottom-1-top"></div>
+        <div class="bottom-1-top-left"></div>
+        <div class="bottom-1-top-right">
+          <div class="bottom-1-top-right-window"></div>
+        </div>
+      </div>
+      <div class="ship-body">
+        <div class="middle-window"></div>
+        <div class="blue-strip-top"></div>
+        <div class="blue-strip-bottom"></div>
+        <div class="ship-body-top-back">
+          <div class="blue-strip-top-half"></div>
+        </div>
+        <div class="chimney-base">
+          <div class="chimney"></div>
+          <div class="chimney"></div>
+        </div>
+      </div>
+      <div class="ship-body-top-front">
+        <div class="top-antenna"></div>
+        <div class="circular-base"></div>
+        <div class="circular-base-1"></div>
+      </div>
+      <div class="ship-body-top-front-mirror"></div>
+    </div>
+  </div>
+  </div>
+</div>
 </body>
 </html>
